e0fb4bd20b4ea5e089745ced25d3cbf19427e83f
fabriziobagala <fabrizio.bagala@outlook.com>
2025-05-02 16:39:52 +0200

    Add LoggingBehavior to centralize request logging

--- src/TradingService.Application/Logging/ApplicationLogging.cs
+++ src/TradingService.Application/Logging/ApplicationLogging.cs
@@ -4,6 +4,11 @@ namespace TradingService.Application.Logging;
 
 public static partial class ApplicationLogging
 {
+    [LoggerMessage(EventName = "RequestHandling", Level = LogLevel.Information, Message = "Handling {RequestName} with payload: {@Request}")]
+    public static partial void LogRequestHandling(this ILogger logger, string requestName, object request);
+
+    [LoggerMessage(EventName = "RequestHandled", Level = LogLevel.Information, Message = "Handled {RequestName}")]
+    public static partial void LogRequestHandled(this ILogger logger, string requestName);
 
     [LoggerMessage(EventName = "NoTradesFound", Level = LogLevel.Warning, Message = "No trades found for the given page number: {PageNumber} and size: {PageSize}.")]
     public static partial void LogNoTradesFound(this ILogger logger, int pageNumber, int pageSize);
